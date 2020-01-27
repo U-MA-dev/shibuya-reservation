34cf452129e5901a032b6442328091cd99547920
yuma_kawabata <kawabata@iotbase.co.jp>
2020-01-27 16:52:55 +0900

    Revert "check duplicated"
    This reverts commit a4bf06ced473fbeca128210a3dbe2b58e58d220e.

--- src/pages/api/register-reserve.js
+++ src/pages/api/register-reserve.js
@@ -15,65 +15,21 @@ export default async (req, res) => {
   );
   const db = client.db(databaseConfig.DB_NAME);
 
-  let isExists = false;
-  await db
-    .collection(databaseConfig.RESERVE_COLLECTION_NAME)
-    .aggregate([
-      {
-        $match: {
-          reserved_at: req.body.reservedAt,
-          place: req.body.classroom,
-          seat_id: req.body.seatId
-        }
-      },
-      {
-        $lookup: {
-          from: databaseConfig.SPACE_COLLECTION_NAME,
-          localField: "space_id",
-          foreignField: "_id",
-          as: "space"
-        }
-      },
-      {
-        $lookup: {
-          from: databaseConfig.TIME_ZONE_COLLECTION_NAME,
-          localField: "time_zone_id",
-          foreignField: "_id",
-          as: "time_zone"
-        }
-      }
-    ])
-    .toArray()
-    .then(docs => {
-      if (docs.length > 0) {
-        isExists = true;
-      }
-    })
-    .catch(err => {
-      res.status(500).json({});
-      console.log(err);
-    });
-
-  if (isExists) {
-    const timeZoneRes = await db
-      .collection(databaseConfig.TIME_ZONE_COLLECTION_NAME)
-      .insert({ time_zone: req.body.timeZone });
-    const spaceRes = await db
-      .collection(databaseConfig.SPACE_COLLECTION_NAME)
-      .insert({ place: req.body.classroom, seat_id: req.body.seatId });
-    await db.collection(databaseConfig.RESERVE_COLLECTION_NAME).insert({
-      reserved_at: req.body.reservedAt,
-      name: req.body.name,
-      email: req.body.email,
-      time_zone_id: timeZoneRes.ops[0]._id,
-      space_id: spaceRes.ops[0]._id,
-      created_at: str_now,
-      updated_at: str_now
-    });
-    res.status(200);
-  } else {
-    res.status(500);
-  }
+  const timeZoneRes = await db
+    .collection(databaseConfig.TIME_ZONE_COLLECTION_NAME)
+    .insert({ time_zone: req.body.timeZone });
+  const spaceRes = await db
+    .collection(databaseConfig.SPACE_COLLECTION_NAME)
+    .insert({ place: req.body.classroom, seat_id: req.body.seatId });
+  await db.collection(databaseConfig.RESERVE_COLLECTION_NAME).insert({
+    reserved_at: req.body.reservedAt,
+    name: req.body.name,
+    email: req.body.email,
+    time_zone_id: timeZoneRes.ops[0]._id,
+    space_id: spaceRes.ops[0]._id,
+    created_at: str_now,
+    updated_at: str_now
+  });
   client.close();
 
   res.end();
